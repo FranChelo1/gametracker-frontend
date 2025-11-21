901adc2137f341bd14d156ee74eaeaf173407fa7
FranChelo1 <tommymarley63@gmail.com>
2025-11-21 10:17:31 -0300

    Configurar GitHub Actions para deploy

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// ⚠️ IMPORTANTÍSIMO
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:4000",
-        changeOrigin: true,
-      },
-    },
-  },
+  base: "/gametracker-frontend/",  // EL NOMBRE EXACTO DEL REPO
 });
